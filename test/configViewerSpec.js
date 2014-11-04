43601a94dee0df2d41c3856acf44bb6a9a6f904d
Dave Lockhart <dave.lockhart@d2l.com>
2014-11-04 15:10:57 -0500

    removing support for environment variable

--- test/configViewerSpec.js
+++ test/configViewerSpec.js
@@ -8,16 +8,6 @@ var opts = { configPath: path.join( dataPath, 'config' ) };
 
 describe( 'Config Viewer', function() {
 
-	var oldEnv;
-
-	beforeEach( function() {
-		oldEnv = process.env.CONFIG_PATH;
-	} );
-
-	afterEach( function() {
-		process.env.CONFIG_PATH = oldEnv;
-	} );
-
 	it( 'should load existing config', function( done ) {
 		configViewer.readConfig( 'test1', opts )
 			.then( function( value ) {
@@ -37,19 +27,18 @@ describe( 'Config Viewer', function() {
 			} );
 	} );
 
-	it( 'should throw if config file does not exist', function( done ) {
-		configViewer.readConfig( 'doesnotexist', opts )
+	it( 'should throw if config is not passed in', function( done ) {
+		configViewer.readConfig( 'doesnotexist' )
 			.fail( function( err ) {
 				expect( err ).not.toBeNull();
 				done();
 			} );
 	} );
 
-	it( 'should use CONFIG_PATH if no option specified', function( done ) {
-		process.env.CONFIG_PATH = opts.configPath;
-		configViewer.readConfig( 'test1' )
-			.then( function( value ) {
-				expect( value ).toEqual( { foo: "bar" } );
+	it( 'should throw if config file does not exist', function( done ) {
+		configViewer.readConfig( 'doesnotexist', opts )
+			.fail( function( err ) {
+				expect( err ).not.toBeNull();
 				done();
 			} );
 	} );
