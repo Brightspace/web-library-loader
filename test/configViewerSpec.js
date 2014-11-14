f78ea4ffa269103f657488bb76f7519aae6955bb
Noel Stonehouse <noel.stonehouse@D2L.com>
2014-11-14 10:53:48 -0500

    Add synchronous config viewer.

--- test/configViewerSpec.js
+++ test/configViewerSpec.js
@@ -6,7 +6,7 @@ var configViewer = require('../lib/configViewer'),
 var dataPath = path.join( __dirname, 'data' );
 var opts = { configPath: path.join( dataPath, 'config' ) };
 
-describe( 'Config Viewer', function() {
+describe( 'Config Viewer (Asynchronous)', function() {
 
 	it( 'should load existing config', function( done ) {
 		configViewer.readConfig( 'test1', opts )
@@ -41,6 +41,37 @@ describe( 'Config Viewer', function() {
 				expect( err ).not.toBeNull();
 				done();
 			} );
+	} );
+
+} );
+
+describe( 'Config Viewer (Synchronous)', function() {
+
+	it( 'should load existing config', function( ) {
+		expect(
+				configViewer.readConfigSync( 'test1', opts )
+			).toEqual( { foo: "bar" } );
+	} );
+
+	it( 'should throw if config path does not exist', function() {
+		expect( function() {
+				configViewer.readConfigSync(
+					 	'doesnotexist',
+						{ configPath: path.join( dataPath, 'doesnotexist' ) }
+					)
+			}).toThrow();
+	} );
+
+	it( 'should throw if config is not passed in', function() {
+		expect( function() {
+				configViewer.readConfigSync( 'doesnotexist' )
+			}).toThrow();
+	} );
+
+	it( 'should throw if config file does not exist', function() {
+		expect( function() {
+				configViewer.readConfigSync( 'doesnotexist', opts )
+			} ).toThrow();
 	} );
 
 } );
